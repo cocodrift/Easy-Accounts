133f7e186e3f56ceff1597f055ffc5b168725c75
mark mwaniki <68171024+cocodrift@users.noreply.github.com>
2026-01-16 16:11:56 +0300

    Update app.js
    Export the app for use in other modules.

--- app.js
+++ app.js
@@ -37,5 +37,6 @@ app.get('/', (req, res) => {
 app.use('/auth', authRoutes)
 app.use('/ledger', ledgerRoutes)
 
-app.listen(3000, () => console.log('Server running on http://localhost:3000'))
+export default app
+
 
